310dad8e244b874fac889697a34c529d43f5702b
Rajni <samjmu85@gmail.com>
2025-09-30 13:19:03 +0530

    updated to version 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is version 2
+          This is version 3
         </p>
         <a
           className="App-link"
